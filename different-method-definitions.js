eba8d1e091ab2a0725958a473b708bf306b400d4
DonCastillo <don.qcastillo@yahoo.com>
2020-12-23 15:01:48 -0700

    added file

---
+++ different-method-definitions.js
@@ -0,0 +1,25 @@
+/**
+ *  Different ways of defining a method
+ */
+
+ const simpleArithmetic = {
+
+    // usual way, using key-value
+    add: function(x, y) {
+        return x + y;
+    }, 
+
+    // function name() way
+    subtract (x, y) {
+        return x - y;
+    },
+
+    // arrow function
+    multiply = (x, y) => x * y
+
+
+ };
+
+
+ console.log( simpleArithmetic.add( 10, 5 ) );          // 15
+ console.log( simpleArithmetic.subtract( 10, 5 ) );     // 5
