15f0c83541bae2bf5f38c4166840a53c27990dea
DonCastillo <don.qcastillo@yahoo.com>
2021-01-14 02:21:15 -0700

    Update index.js

--- first-app/index.js
+++ first-app/index.js
@@ -3,8 +3,19 @@ const app = express();
 
 //console.dir(app);
 
-app.use(() => {
+/**
+ *  @param  req     request
+ *  @param  res     response
+ *  these are default params
+ */
+app.use((req, res) => {
     console.log('We got a new request!');
+    // res.send('Hello, we got your request. This is our response');
+        // returns a plain text
+    // res.send({color: 'red'})
+        // returns as a json object
+    // res.send('<h1>This is my webpage</h1>')
+        // returns as a markup
 });
 
 app.listen(3000, () => {
