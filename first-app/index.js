4099138277491026d472b9f73fc6331e3160b30c
DonCastillo <don.qcastillo@yahoo.com>
2021-01-14 17:22:45 -0700

    Update index.js

--- first-app/index.js
+++ first-app/index.js
@@ -8,15 +8,39 @@ const app = express();
  *  @param  res     response
  *  these are default params
  */
-app.use((req, res) => {
-    console.log('We got a new request!');
-    // res.send('Hello, we got your request. This is our response');
-        // returns a plain text
-    // res.send({color: 'red'})
-        // returns as a json object
-    // res.send('<h1>This is my webpage</h1>')
-        // returns as a markup
+// app.use((req, res) => {
+//     console.log('We got a new request!');
+//     // res.send('Hello, we got your request. This is our response');
+//         // returns a plain text
+//     // res.send({color: 'red'})
+//         // returns as a json object
+//     // res.send('<h1>This is my webpage</h1>')
+//         // returns as a markup
+// });
+
+app.get('/', (req, res) => {
+    res.send('This is the home page');
 });
+
+app.get('/cats', (req, res) => {
+    console.log("CAT REQUEST!!");
+    res.send('MEOW');
+});
+
+app.post('/cats', (req, res) => {
+    res.send('POST REQUEST TO /cats');
+});
+
+app.get('/dogs', (req, res) => {
+    console.log("DOG REQUEST!!");
+    res.send('WOOF');
+});
+
+app.get('*', (req, res) => {
+    res.send('I don\'t know that path');
+});
+
+
 
 app.listen(3000, () => {
     console.log('listening on port 3000!');
