222d6832c8a7a3c041c8655b4aa92f0d6934b9ac
DonCastillo <don.qcastillo@yahoo.com>
2021-02-11 01:45:33 -0700

    added post route

--- rest-demo/index.js
+++ rest-demo/index.js
@@ -2,18 +2,49 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const data = require('./data.json')
+const fs = require('fs')
 
-
+// accept data sent from x-www-form-urlencoded
 app.use(express.urlencoded({extended: true}))
+// accept data as application/json
 app.use(express.json())
 app.set('views', path.join(__dirname, 'views'))
+// use ejs templating
 app.set('view engine', 'ejs')
 
 
 
 
+const read = () => {
+    let file = fs.readFileSync('./data.json')
+    return JSON.parse(file)
+}
+
+const write = (username, comment) => {
+    let file = read()
+    file.comments.push({username: username, comment: comment})
+    fs.writeFileSync('./data.json', JSON.stringify(file))
+}
+
+
+
+// lists all comments
 app.get('/comments', (req, res) => {
+    const data = read()
+    console.log(data)
     res.render('comments/index', {data})
+})
+
+// where the form lives
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new')
+})
+
+// creates new comment
+app.post('/comments', (req, res) => {
+    const {username, comment} = req.body
+    write(username, comment)
+    res.send('It worked')
 })
 
 app.get('/tacos', (req, res) => {
