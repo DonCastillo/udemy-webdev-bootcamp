2fb04120e6cadd8d2173094825c28ee565b930c9
DonCastillo <don.qcastillo@yahoo.com>
2021-04-25 01:31:09 -0600

    able to delete and filter products

--- express_mongoose/index.js
+++ express_mongoose/index.js
@@ -36,8 +36,15 @@ app.get('/products/new', (req, res) => {
 })
 
 app.get('/products', async (req, res) => {
-    const products = await Product.find({})
-    res.render('products/index', {products})
+    const {category} = req.query
+    if (category) {
+        const products = await Product.find({category})
+        res.render('products/index', {products, category})
+    } else {
+        const products = await Product.find({})
+        res.render('products/index', {products, category: 'All'})
+    }
+    
 })
 
 app.get('/products/:id', async (req, res) => {
@@ -56,6 +63,12 @@ app.put('/products/:id', async (req, res) => {
     const {id} = req.params
     const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
     res.redirect(`/products/${product._id}`)
+})
+
+app.delete('/products/:id', async (req, res) => {
+    const {id} = req.params
+    const deletedProduct = await Product.findByIdAndDelete(id)
+    res.redirect('/products')
 })
 
 app.listen(3000, () => {
